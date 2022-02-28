74bb053aaa7da1f21252567ecd768408a28de5b3
Allison Stewart <amstewart@yahoo.com>
2022-02-28 18:17:56 -0500

    nav-bar working

--- cookbook/src/components/Navbar/index.js
+++ cookbook/src/components/Navbar/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  const { token, isLoggedIn, setIsLoggedIn } = props; //CHANGE WHEN WE START REDUX
+  // const { token, isLoggedIn, setIsLoggedIn } = props; //CHANGE WHEN WE START REDUX
 
-  useEffect(() => {
-    token !== null ? setIsLoggedIn(true) : setIsLoggedIn(false);
-  }, [token, setIsLoggedIn]);
+  // useEffect(() => {
+  //   token !== null ? setIsLoggedIn(true) : setIsLoggedIn(false);
+  // }, [token, setIsLoggedIn]);
 
   return (
     <header className="header-container">
@@ -13,13 +14,21 @@ const Navbar = () => {
         <Link className="link" to="/">F.CB</Link>
       </div>
       <nav className="nav-links">
-        {isLoggedIn && <Link className="link" to="/about">About Us</Link>}
-        {isLoggedIn && <Link className="link" to="/categories">Categories</Link>}
-        {!isLoggedIn && <Link className="link" to="/account">My Account</Link>}
-        {!isLoggedIn && <Link className="link" to="/logout">Logout</Link>}
+        {<Link className="link" to="/about">About Us</Link>}
+        {<Link className="link" to="/categories">Categories</Link>}
+        {<Link className="link" to="/account">My Account</Link>}
+        {<Link className="link" to="/logout">Logout</Link>}
       </nav>
     </header>
   );
 };
 
 export default Navbar;
+
+
+{/* <nav className="nav-links">
+{isLoggedIn && <Link className="link" to="/about">About Us</Link>}
+{isLoggedIn && <Link className="link" to="/categories">Categories</Link>}
+{!isLoggedIn && <Link className="link" to="/account">My Account</Link>}
+{!isLoggedIn && <Link className="link" to="/logout">Logout</Link>}
+</nav> */}
