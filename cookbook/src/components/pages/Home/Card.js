9b947848392f2749cbc44eab55159d93034eacbf
Christwide Oscar <triple101.games@gmail.com>
2022-03-03 00:11:10 -0600

    Card and Home | WIP

--- cookbook/src/components/pages/Home/Card.js
+++ cookbook/src/components/pages/Home/Card.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-function Card() {
+function Card(props) {
     return (
         <div className="hCard">
-            <div className="hCardBody">
-                <img src="" />
-                <h2 className="hCardTitle"></h2>
-                <p className="hCardDes"></p>
+            <div className="hCard_Body">
+                <img src={props.img}/>
+                <h2 className="hCard_Title">{props.title}</h2>
+                <p className="hCard_Des">{props.des}</p>
             </div>
-            <button className="hCardButton"> View Recipe </button>
+            <button className="hCard_Button"> View Recipe </button>
         </div>
     )
 }
