7c2be183a8ee1f9e09c5ffdc362f6e16386e45b5
Christwide Oscar <triple101.games@gmail.com>
2022-03-02 20:49:59 -0600

    HomeScreen WIP

--- cookbook/src/App.js
+++ cookbook/src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
-
-import Navbar from './components/Navbar';
-import About from './components/pages/About';
-import Categories from './components/pages/Categories';
-import Login from './components/pages/Login';
-import Register from './components/pages/Register';
-import Logout from './components/pages/Logout'
+import Navbar from "./components/Navbar";
+import About from "./components/pages/About";
+import Categories from "./components/pages/Categories";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import Logout from "./components/pages/Logout";
+import Home from "./components/pages/Home/Home";
 
 function App() {
   return (
@@ -15,13 +15,12 @@ function App() {
       <Navbar />
 
       <Routes>
+        <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<About />} />
         <Route exact path="/categories" element={<Categories />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/register" element={<Register />} />
         <Route exact path="/logout" element={<Logout />} />
-
-
       </Routes>
       <h1>COOK BOOK BUDS</h1>
     </div>
---
+++ cookbook/src/components/pages/Home/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Card() {
+    return (
+        <div className="hCard">
+            <div className="hCardBody">
+                <img src="" />
+                <h2 className="hCardTitle"></h2>
+                <p className="hCardDes"></p>
+            </div>
+            <button className="hCardButton"> View Recipe </button>
+        </div>
+    )
+}
+
+export default Card;
