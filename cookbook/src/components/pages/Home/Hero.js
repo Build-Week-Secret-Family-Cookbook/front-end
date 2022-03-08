563dcaeb839dbcd6a8578ab3aecab6da364c6489
Christwide Oscar <triple101.games@gmail.com>
2022-03-08 16:55:11 -0600

    Creating Hero

--- cookbook/src/components/pages/Home/Hero.js
+++ cookbook/src/components/pages/Home/Hero.js
@@ -1,20 +1,30 @@
 import React from "react";
+import "./Hero.css";
 
 function Hero() {
-    return (
-        <div className="Wrapper">
-            <div className="h_Section1">
-                <div className="h_Picture"></div>
-                <h1>Hero Section </h1>
-                <div className="h_Para">
-                <p>Every year on March 8th we celebrate International Women’s Day to highlight the historical, cultural, social, and political achievements of women. 
-                </p>
-                <button className="button"> Learn More</button>
-            </div>
-            </div>
-            
+  return (
+    <div className="h_Wrapper">
+      <div className="h_Section1">
+        <div className="h_Picture"></div>
+        <div className="h_Title">
+          <h1>Hero Section </h1>
         </div>
-    )
+        <div className="h_Para">
+          <p>
+            Every year on March 8th we celebrate International Women’s Day to
+            highlight the historical, cultural, social, and political
+            achievements of women.
+          </p>
+          <button className="button"> Learn More</button>
+        </div>
+      </div>
+      <div className="h_Section2">
+          <div className="h_picture">
+            <h1> </h1>
+          </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
